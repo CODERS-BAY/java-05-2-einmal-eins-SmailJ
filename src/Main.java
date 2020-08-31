9076284a45e2d9d5bed7fd7f397dbceddfa87fb0
Smail <smail.jusic16@gmail.com>
2020-08-31 11:30:12 +0200

    printf eingefügt

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,9 @@ public class Main {
 
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                int result = j*i;
-                System.out.println(j+" x "+i+" = "+result);
+                int result = j * i;
+                System.out.printf("%d x %d = " + result, j, i);
+                System.out.println();
             }
         }
     }
